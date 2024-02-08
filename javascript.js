710eca76ebd1d77376e1567fb9b846d6e81d84a5
DK <dzilla398@gmail.com>
2024-02-08 14:04:38 +1030

    Created initial layout of calculator in css

---
+++ javascript.js
@@ -0,0 +1,22 @@
+let add = (a,b) => a + b;
+
+
+let subtract = (a,b) => a - b;
+
+let multiply = (a,b) => a*b;
+
+
+let divide = (a,b) => a/b;
+
+
+let operate = (a,b) => {
+    a = 5;
+    b = 10;
+    console.log(add(a,b));
+    console.log(subtract(a,b));
+    console.log(multiply(a,b));
+    console.log(divide(a,b));
+}
+
+operate();
+
