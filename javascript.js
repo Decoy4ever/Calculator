89a5414ff3b4dc8cd209d1780d7348466ed0d772
David Koy <dzilla398@gmail.com>
2024-08-19 11:39:06 +0930

    modified js file to display clicked 'numbers' iteratively
    modified the html file by changing the class name of the div tags

--- javascript.js
+++ javascript.js
@@ -1,51 +1,72 @@
+const operatorButton = document.querySelectorAll('.operator')
+const numberButton = document.querySelectorAll('.number')
 const additionButton = document.querySelector('.add')
 const subtractButton = document.querySelector('.sub')
 const multiplicationButton = document.querySelector('.mult')
 const divideButton = document.querySelector('.divide')
 const equalButton = document.querySelector('.equal')
-const displayDiv = document.querySelector('.display')
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('button')
 
 // Store the numbers and operators in an empty array
-let firstNum = []
-let secondNum = []
-let operator = []
+let displayValue = ""
+let mathOperationArr = ""
+
 
 function add(num1,num2)
 {
-    console.log(num1 + num2)
+    return num1 + num2
 }
 
 function subtract(num1,num2)
 {
-    console.log(num1 - num2)
+    return num1 - num2
 }
 function multiply(num1,num2)
 {
-    console.log(num1 * num2)
+    return num1 * num2
 }
 
 function divide(num1,num2)
 {
-    console.log(num1 / num2)
+    return num1 / num2
 }
 
-const btn = document.querySelector('.container-item')
-
-function displayOperations()
+let numberArr = ""
+function getNumberButtons()
 {
-    btn.addEventListener('click',(e) =>
+    function numbers()
     {
-        console.log(e.target.textContent)
-        // change the text content of the display
-        displayDiv.textContent = e.target.textContent
-    }) 
+        numberButton.forEach((btn) =>
+        {
+            btn.addEventListener('click',(e) =>
+            {
+                display.textContent = e.target.textContent
+                
+                // store the value the click element into a var
+                displayValue = display.textContent
+                
+                // join the clicked element into a another var
+                numberArr = numberArr.concat(displayValue)
+                console.log(numberArr)
+                
+                // update the display
+                display.textContent = numberArr
+            })
+        })
+    }
+    return numbers
+}
+     
+function operate(firstNumber, secondNumber)
+{
+    firstNumber = getNumberButtons()
+    // secondNumber = getNumberButtons()
+    return firstNumber
 }
 
-displayOperations()
-
-
-
-
+const getMathOperation = operate()
+getMathOperation()
 
 
 
