d7f7dc5b4477a608ac6d4792950cc9e82d096707
David Koy <dzilla398@gmail.com>
2024-08-22 12:25:29 +0930

    removed class name in div class in index.html
    modified javascript file to enable basic math functionality when user clicks on buttons
    
    removed comments in stylesheet.css

--- javascript.js
+++ javascript.js
@@ -1,72 +1,181 @@
-const operatorButton = document.querySelectorAll('.operator')
-const numberButton = document.querySelectorAll('.number')
-const additionButton = document.querySelector('.add')
-const subtractButton = document.querySelector('.sub')
-const multiplicationButton = document.querySelector('.mult')
-const divideButton = document.querySelector('.divide')
-const equalButton = document.querySelector('.equal')
-const display = document.querySelector('.display')
-const buttons = document.querySelectorAll('button')
-
-// Store the numbers and operators in an empty array
-let displayValue = ""
-let mathOperationArr = ""
-
+/**
+ *  1. Create Basic Math Operations for Calculator
+ */
 
 function add(num1,num2)
 {
-    return num1 + num2
+    return parseInt(num1) + parseInt(num2)
 }
 
 function subtract(num1,num2)
 {
-    return num1 - num2
+    return parseInt(num1 - num2)
 }
+
 function multiply(num1,num2)
 {
-    return num1 * num2
+    return parseInt(num1 * num2)
 }
 
 function divide(num1,num2)
 {
-    return num1 / num2
+    return parseInt(num1/num2)
 }
 
-let numberArr = ""
-function getNumberButtons()
+/**
+ * 2. Create 3 variables used to update the display
+ */
+let firstNumber = ""
+let secondNumber = ""
+let operator = ""
+
+/** 
+ * Create Operation function that take 3 parameters and calls one above function
+*/
+
+function operate(num1,num2,op)
 {
-    function numbers()
+   if(op === '+')
+   {
+        return add(num1,num2)
+   }
+   else if(op === '-')
+   {
+        return subtract(num1,num2)
+   }
+   else if(op === '/')
     {
-        numberButton.forEach((btn) =>
+         return divide(num1,num2)
+    }
+    else if(op === '*')
+    {
+        return multiply(num1,num2)
+    }
+}
+
+/**
+ * Create function populates the display
+ */
+
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('button')
+const clearButton = document.querySelector('.clear')
+let reset = ""
+
+function getNumpad()
+{
+    buttons.forEach((btn) => 
+    {
+        // handle click number button event
+        btn.addEventListener('click',(e) =>
         {
-            btn.addEventListener('click',(e) =>
+            if(btn.className === 'number')
             {
+                if(operator === "")
+                {
+                    // display the current click event
+                    display.textContent = e.target.textContent
+
+                    // update the display to show the current click event to handle multiple clicked events
+                    firstNumber = firstNumber + display.textContent
+
+                    // update the display
+                    display.textContent = firstNumber
+                }
+                else if(operator !== "")
+                {
+                       // display the current click event
+                       display.textContent = e.target.textContent
+
+                       // update the display to show the current click event to handle multiple clicked events
+                       secondNumber = secondNumber + display.textContent
+   
+                       // update the display
+                       display.textContent = secondNumber
+                }
+            }
+       
+            console.log('first number: ' + firstNumber)
+            console.log('second number: ' + secondNumber)
+
+            if(btn.className === 'operator')
+            {
+                if(btn.textContent === '+')
+                {
+                    display.textContent = e.target.textContent
+
+                    operator = display.textContent
+                }
+                else if(btn.textContent === '-')
+                {
+                    display.textContent = e.target.textContent
+
+                    operator = display.textContent
+                }
+                else if(btn.textContent === '*')
+                {
+                    display.textContent = e.target.textContent
+
+                    operator = display.textContent
+                }
+                else if(btn.textContent === '/')
+                {
+                    display.textContent = e.target.textContent
+
+                    operator = display.textContent
+                }
+            }
+            console.log('operator: ' + operator)
+
+            if(btn.className === 'equal')
+            {
+                // display the '=' sign
                 display.textContent = e.target.textContent
                 
-                // store the value the click element into a var
-                displayValue = display.textContent
-                
-                // join the clicked element into a another var
-                numberArr = numberArr.concat(displayValue)
-                console.log(numberArr)
-                
-                // update the display
-                display.textContent = numberArr
-            })
+                // display the result of the operator
+                operator = operate(firstNumber,secondNumber,operator)
+                console.log(operator)
+
+                display.textContent = operator
+            }
+
+            if(btn.className === 'clear')
+            {
+                display.textContent = ""
+                firstNumber = ""
+                secondNumber = ""
+                op = ""
+            }
         })
-    }
-    return numbers
-}
-     
-function operate(firstNumber, secondNumber)
-{
-    firstNumber = getNumberButtons()
-    // secondNumber = getNumberButtons()
-    return firstNumber
+    })
 }
 
-const getMathOperation = operate()
-getMathOperation()
+const number = getNumpad()
+number
+
+
+
+
+
+
+
+ 
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
