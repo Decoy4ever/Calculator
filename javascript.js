abf184ccf6b3fd04d129b742a268394f33d5736a
David Koy <dzilla398@gmail.com>
2024-09-03 14:58:16 +0930

    Modified button tag that contains a class 'decimal' to 'number'. Added id attr to this button
    Modified the index.html to display floating numbers instead of strings.
    
    Removed the decimalButton function and refactored the logic into the getNumberButton function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
-/**
- *  1. Create Basic Math Operations for Calculator
- */
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('button')
+const decimalButton = document.querySelector('#decimal')
+let sum = ""
+let firstNumber = ""
+let secondNumber = ""
+let operator = ""
 
 function add(num1,num2)
 {
@@ -30,17 +34,6 @@ function divide(num1,num2)
     }
 }
 
-/**
- * 2. Create 3 variables used to update the display
- */
-let firstNumber = ""
-let secondNumber = ""
-let decimal = ""
-let operator = ""
-
-/** 
- * Create Operation function that take 3 parameters and calls one above function
-*/
 
 function operate(num1,num2,op)
 {
@@ -61,15 +54,6 @@ function operate(num1,num2,op)
         return multiply(num1,num2)
     }
 }
-
-/**
- * Create function populates the display
- */
-
-const display = document.querySelector('.display')
-const buttons = document.querySelectorAll('button')
-const decimalButton = document.querySelector('.decimal')
-let sum = ""
 
 
 function getNumpad()
@@ -92,10 +76,6 @@ function getNumpad()
             {
                 getEqualButton(e)
             }
-            else if(btn.className === 'decimal')
-            {
-                getDecimalButton(e)
-            }
             else if(btn.className === 'delete')
             {
                 getDeleteNumberButton(e)
@@ -108,70 +88,60 @@ function getNumpad()
     })
 }
 
-function getClearAllButton(clearAll)
-{
-    display.textContent = clearAll.target.textContent
-    display.textContent = "0"
-    console.log('I clicked clear all')
-    firstNumber = ""
-    secondNumber = ""
-    operator = ""
-    console.log('firstNumber: ' + firstNumber + ' secondNumber: ' + secondNumber)
-}
-
-
-function getDecimalButton(dec)
-{
-    display.textContent = dec.target.textContent
-    if(firstNumber.includes('0') || secondNumber.includes('0'))
-    {
-        firstNumber = "0"
-        secondNumber = "0"
-    }
-
-    if(operator === "")
-    {
-        firstNumber = firstNumber + display.textContent
-        console.log('firstNumber as a floating point: ' + firstNumber)
-        display.textContent = firstNumber
-    }
-    else if(operator !== "")
-    {
-        secondNumber = secondNumber + display.textContent
-        console.log('secondNumber as a floating point: ' + secondNumber)
-        display.textContent = firstNumber + operator + secondNumber 
-    }
- 
-    // if firstBumber includes a '.' will disable the '.' button
-    if(firstNumber.includes('.') || secondNumber.includes('.'))
-    {
-        decimalButton.disabled = true
-    }
-}
 
 function getNumberButton(number)
 {
-    // display the targeted click event
+    // display the initial targeted click event
     display.textContent = number.target.textContent 
 
-    if(operator === "")
+    if(secondNumber === "" && operator === "")
     {
-        // update the display to show the current click event to handle multiple clicked events
         firstNumber = firstNumber + display.textContent
 
-        // update the display
-        display.textContent = firstNumber 
+        // if user selects '.' print NaN to overcome this do the following operation 
+        if(isNaN(firstNumber))
+        {
+            firstNumber = "0" + firstNumber
+        }
+
+        display.textContent = parseFloat(firstNumber) 
     }
-    else if(operator !== "")
+    else if(firstNumber !== "" && operator !== "")
     {
         secondNumber = secondNumber + display.textContent
 
-        // update the display to include firstNumber and secondNumber
-        display.textContent = firstNumber + operator + secondNumber
+        if(isNaN(secondNumber))
+        {
+            secondNumber = "0" + secondNumber
+        }
+        display.textContent = parseFloat(firstNumber) + operator + parseFloat(secondNumber)
     }
-    console.log('first number: ' + firstNumber)
-    console.log('second number: ' + secondNumber)
+    
+    // Handle the disabling of buttons
+    if(firstNumber.includes('.') && operator === "")
+    {
+        decimalButton.disabled = true
+    }
+    else if(secondNumber.includes('.') && operator !== "" )
+    {
+        decimalButton.disabled = true
+    }
+    console.log(`firstNumber: ${firstNumber}`)
+    console.log(`secondNumber: ${secondNumber}`)
 
+}
+
+
+function getClearAllButton(clearAll)
+{
+    clearAll.target.textContent
+    console.log('I clicked clear all')
+    firstNumber = "0"
+    secondNumber = ""
+    operator = ""
+    console.log('firstNumber: ' + firstNumber + ' secondNumber: ' + secondNumber)
+    display.textContent = firstNumber
+    decimalButton.disabled = false
 }
 
 function getOperatorButton(op)
@@ -180,55 +150,55 @@ function getOperatorButton(op)
     if(firstNumber !== "" && secondNumber === "")
     {
         operator = display.textContent
-        display.textContent = firstNumber + operator 
+        display.textContent = parseFloat(firstNumber) + operator 
         console.log('operator: ' + operator)
         decimalButton.disabled = false
         console.log('decimal button is enabled again after clicking operator')
     }
+    // handles case where firstNumber = secondNumber = 0
     else if(firstNumber === "" && secondNumber === "")
     {
         firstNumber = "0"
-        secondNumber = ""
+        secondNumber = "0"
         operator = display.textContent
-        display.textContent = firstNumber + operator 
+        display.textContent = parseFloat(firstNumber) + operator 
         decimalButton.disabled = false
     }
-
 }
-
 
 function getEqualButton(equalSign)
 {
     display.textContent = equalSign.target.textContent
     if(firstNumber !== "" && secondNumber !== "" && operator !== "")
     {
-        display.textContent = firstNumber + operator + secondNumber
+        display.textContent = parseFloat(firstNumber) + operator + parseFloat(secondNumber)
         sum = operate(firstNumber,secondNumber,operator)
         console.log('Sum: ' + sum)
-    
-        display.textContent = sum
+        display.textContent = parseFloat(sum)
+        decimalButton.disabled = false
+
         // store the sum as the firstNumber and display
         firstNumber = sum 
         secondNumber = ""
+        operator = ""
+    }
+    else if(firstNumber !== "" && secondNumber === "" && operator === "")
+    {
+        return display.textContent = firstNumber
     }
 }
+
 
 function getDeleteNumberButton(del)
 {
     del.target.className
-
-
     if(firstNumber !== "" && operator === "" && secondNumber === "")
     {
-        // handle case to delete firstNumber
-        console.log(firstNumber.length)
         let len1 = firstNumber.length
-        // let deletedFirstNumber = ""
+        console.log(firstNumber.length)
         firstNumber = firstNumber.slice(0,len1 - 1)
-        
-        // firstNumber = deletedFirstNumber
-        console.log('deleted firstNumber: ' + firstNumber)
-        display.textContent = firstNumber
+        console.log('firstNumber becomes: ' + firstNumber)
+        display.textContent = parseFloat(firstNumber)
 
         if(firstNumber === "")
         {
@@ -237,23 +207,17 @@ function getDeleteNumberButton(del)
     }
     else if(firstNumber !== "" && operator !== "" && secondNumber !== "")
     {
-        // handle case to delete secondNumber
         let len2 = secondNumber.length
-        // let deletedSecondNumber = ""
         secondNumber =  secondNumber.slice(0,len2 - 1)
-        console.log('deleted secondNumbers: ' + secondNumber)
-
-        // secondNumber = deletedSecondNumber
+        console.log('secondNumber becomes: ' + secondNumber)
         display.textContent = firstNumber + operator + secondNumber
     }
     else if(firstNumber !== "" && operator !== "" && secondNumber === "")
     {
-        let deletedOperator = operator.slice(0,-1)
-        operator = deletedOperator
+        operator = operator.slice(0,-1)
         console.log('deleted operator')
-        display.textContent = firstNumber + operator
+        display.textContent = parseFloat(firstNumber) + operator
     }
-    
 }
 
 const number = getNumpad()
