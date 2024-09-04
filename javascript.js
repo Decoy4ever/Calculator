8d0c7e121b0b0e7d9a450eadce16e698219c67ab
David Koy <dzilla398@gmail.com>
2024-09-04 15:07:07 +0930

    modified the layout of the calculator by styling the buttons and colors of the text. Changed default font to 'Suse'.
    Modified the js file by fixing the float to do math operations

--- javascript.js
+++ javascript.js
@@ -13,12 +13,12 @@ function add(num1,num2)
 
 function subtract(num1,num2)
 {
-    return parseFloat(num1) -parseFloat(num2)
+    return parseFloat(num1) - parseFloat(num2)
 }
 
 function multiply(num1,num2)
 {
-    return parseFloat(num1) *  parseFload(num2)
+    return parseFloat(num1) *  parseFloat(num2)
 }
 
 function divide(num1,num2)
